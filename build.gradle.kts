996575a8300080ca781074a8a03956a3ee1e6290
Benedek Szabó <bndkszabo@gmail.com>
2025-05-13 22:09:49 +0200

    Bump app version to 1.29.6 + small edits.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,8 @@ buildscript {
     extra.apply {
         // Cannot be called "ndkVersion" as that leads to naming collision
         // Changes to this value must be reflected in `./docker/Dockerfile`
-        set("ndkVersionShared", "25.2.9519653")
+        //TODO: Learn docker
+        set("ndkVersionShared", "29.0.13113456")
 
     }
 
