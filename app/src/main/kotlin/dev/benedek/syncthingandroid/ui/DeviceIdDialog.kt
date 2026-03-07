87836f69525173f0cd035b4a97866df0b9e688ab
Benedek Szabó <bndkszabo@gmail.com>
2026-03-07 22:11:18 +0100

    Add DeviceIdDialog

---
+++ app/src/main/kotlin/dev/benedek/syncthingandroid/ui/DeviceIdDialog.kt
@@ -0,0 +1,19 @@
+package dev.benedek.syncthingandroid.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import dev.benedek.syncthingandroid.ui.theme.SyncthingandroidTheme
+
+@Composable
+fun DeviceIdDialog() {
+    Text("Empty")
+}
+
+@Preview
+@Composable
+fun DeviceIdDialogPreview() {
+    SyncthingandroidTheme() {
+        DeviceIdDialog()
+    }
+}
