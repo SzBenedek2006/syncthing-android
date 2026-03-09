9a0a2635b571f9e94dd2a2ca487d79105e4161c5
Benedek Szabó <bndkszabo@gmail.com>
2026-03-09 12:32:00 +0100

    Move to Kotlin Coroutine

--- app/src/main/java/dev/benedek/syncthingandroid/activities/LogActivity.kt
+++ app/src/main/java/dev/benedek/syncthingandroid/activities/LogActivity.kt
@@ -1,7 +1,6 @@
 package dev.benedek.syncthingandroid.activities
 
 import android.content.Intent
-import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
@@ -14,11 +13,15 @@ import androidx.appcompat.widget.ShareActionProvider
 import androidx.core.view.MenuItemCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import dev.benedek.syncthingandroid.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
-import java.lang.ref.WeakReference
 
 /**
  * Shows the log information from Syncthing.
@@ -26,7 +29,7 @@ import java.lang.ref.WeakReference
 class LogActivity : SyncthingActivity() {
     private var log: TextView? = null
     private var syncthingLog = true
-    private var fetchLogTask: AsyncTask<*, *, *>? = null
+    private var fetchLogJob: Job? = null
     private var scrollView: ScrollView? = null
     private var shareIntent: Intent? = null
 
@@ -119,96 +122,55 @@ class LogActivity : SyncthingActivity() {
     }
 
     private fun updateLog() {
-        if (fetchLogTask != null) {
-            fetchLogTask!!.cancel(true)
-        }
+
+        fetchLogJob?.cancel()
+
         log!!.setText(R.string.retrieving_logs)
-        fetchLogTask = UpdateLogTask(this).execute()
+        fetchLogJob = lifecycleScope.launch {
+            val logResult = getLogAsync(syncthingLog)
+            log!!.text = logResult
+
+            shareIntent?.putExtra(Intent.EXTRA_TEXT, logResult)
+
+            scrollView!!.post {
+                scrollView!!.scrollTo(0, log!!.bottom)
+            }
+
+
+        }
     }
 
-    private class UpdateLogTask(context: LogActivity?) : AsyncTask<Void?, Void?, String?>() {
-        private val refLogActivity: WeakReference<LogActivity?> = WeakReference<LogActivity?>(context)
-
-        @Deprecated("Deprecated in Java")
-        override fun doInBackground(vararg params: Void?): String {
-            // Get a reference to the activity if it is still there.
-            val logActivity = refLogActivity.get()
-            if (logActivity == null || logActivity.isFinishing) {
-                cancel(true)
-                return ""
-            }
-            return getLog(logActivity.syncthingLog)
-        }
-
-        @Deprecated("Deprecated in Java")
-        override fun onPostExecute(log: String?) {
-            // Get a reference to the activity if it is still there.
-            val logActivity = refLogActivity.get()
-            if (logActivity == null || logActivity.isFinishing) {
-                return
-            }
-            logActivity.log!!.text = log
-            if (logActivity.shareIntent != null) {
-                logActivity.shareIntent!!.putExtra(Intent.EXTRA_TEXT, log)
-            }
-            // Scroll to bottom
-            logActivity.scrollView!!.post {
-                logActivity.scrollView!!.scrollTo(
-                    0,
-                    logActivity.log!!.bottom
+    private suspend fun getLogAsync(syncthingLog: Boolean): String = withContext(Dispatchers.IO) {
+        var process: Process? = null
+        try {
+            val pb = if (syncthingLog) {
+                ProcessBuilder(
+                    "/system/bin/logcat", "-t", "300", "-v", "time", "-s", "SyncthingNativeCode"
+                )
+            } else {
+                ProcessBuilder(
+                    "/system/bin/logcat", "-t", "300", "-v", "time", "*:i ps:s art:s"
                 )
             }
-        }
+            pb.redirectErrorStream(true)
+            process = pb.start()
 
-        /**
-         * Queries logcat to obtain a log.
-         * 
-         * @param syncthingLog Filter on Syncthing's native messages.
-         */
-        fun getLog(syncthingLog: Boolean): String {
-            var process: Process? = null
-            try {
-                val pb: ProcessBuilder?
-                if (syncthingLog) {
-                    pb = ProcessBuilder(
-                        "/system/bin/logcat",
-                        "-t",
-                        "300",
-                        "-v",
-                        "time",
-                        "-s",
-                        "SyncthingNativeCode"
-                    )
-                } else {
-                    pb = ProcessBuilder(
-                        "/system/bin/logcat",
-                        "-t",
-                        "300",
-                        "-v",
-                        "time",
-                        "*:i ps:s art:s"
-                    )
-                }
-                pb.redirectErrorStream(true)
-                process = pb.start()
-                val bufferedReader = BufferedReader(
-                    InputStreamReader(process.inputStream, "UTF-8"), 8192
-                )
-                val log = StringBuilder()
-                var line: String?
-                val sep = System.lineSeparator()
-                while ((bufferedReader.readLine().also { line = it }) != null) {
-                    log.append(line)
-                    log.append(sep)
-                }
-                return log.toString()
-            } catch (e: IOException) {
-                Log.w(TAG, "Error reading Android log", e)
-            } finally {
-                process?.destroy()
+            val bufferedReader = BufferedReader(InputStreamReader(process.inputStream, "UTF-8"), 8192)
+            val log = StringBuilder()
+            var line: String?
+            val sep = System.lineSeparator()
+
+            while (bufferedReader.readLine().also { line = it } != null) {
+                log.append(line)
+                log.append(sep)
             }
-            return ""
+            return@withContext log.toString()
+        } catch (e: IOException) {
+            Log.w(TAG, "Error reading Android log", e)
+        } finally {
+            process?.destroy()
         }
+        return@withContext ""
     }
 
     companion object {
