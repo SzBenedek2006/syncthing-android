09620b1a897c66e227d801e1e7cbd66146446f9e
Benedek Szabó <bndkszabo@gmail.com>
2026-03-11 16:08:40 +0100

    Remove comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,6 @@ buildscript {
         classpath("com.github.ben-manes:gradle-versions-plugin:0.36.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.21")
         classpath("org.jetbrains.kotlin:compose-compiler-gradle-plugin:2.2.21")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
     }
 }
 
