001a9edc312b933f616fff4d1438d602b78fde3f
Benedek Szabó <bndkszabo@gmail.com>
2026-05-20 16:57:28 +0200

    Increment compose bom version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
     implementation("androidx.documentfile:documentfile:1.1.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
     implementation("androidx.activity:activity-compose:1.13.0")
-    implementation(platform("androidx.compose:compose-bom:2026.05.00"))
+    implementation(platform("androidx.compose:compose-bom:2026.05.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
