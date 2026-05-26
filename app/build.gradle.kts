35101149c1bbd1431eee6e6e35d888dd5d925be2
Benedek Szabó <bndkszabo@gmail.com>
2026-05-26 21:06:47 +0200

    Move beta under release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,11 +87,6 @@ configure<ApplicationExtension> {
 
             resValue("string", "app_package_id", "${defaultConfig.applicationId}$applicationIdSuffix")
         }
-        create("beta") {
-            initWith(getByName("release"))
-            applicationIdSuffix = ".beta"
-            resValue("string", "app_package_id", "${defaultConfig.applicationId}$applicationIdSuffix")
-        }
         getByName("release") {
             isMinifyEnabled = true
             isShrinkResources = true
@@ -103,6 +98,11 @@ configure<ApplicationExtension> {
                 .getOrNull()
                 .takeIf { it?.storeFile != null }
             resValue("string", "app_package_id", "${defaultConfig.applicationId}")
+        }
+        create("beta") {
+            initWith(getByName("release"))
+            applicationIdSuffix = ".beta"
+            resValue("string", "app_package_id", "${defaultConfig.applicationId}$applicationIdSuffix")
         }
     }
 
