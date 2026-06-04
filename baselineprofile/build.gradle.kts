569742028ac2aa6d3d38714dfc9b817e295f1bc2
Benedek Szabó <bndkszabo@gmail.com>
2026-06-04 08:32:09 +0200

    Add baselineprofile module

---
+++ baselineprofile/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.test")
+    id("androidx.baselineprofile")
+}
+
+val benchmarkVersion = rootProject.extra.get("benchmarkVersion") as String
+
+android {
+    namespace = "dev.benedek.baselineprofile"
+    compileSdk = 36
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    defaultConfig {
+        minSdk = 28
+        targetSdk = 36
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    targetProjectPath = ":app"
+
+}
+
+// This is the configuration block for the Baseline Profile plugin.
+// You can specify to run the generators on a managed devices or connected devices.
+baselineProfile {
+    useConnectedDevices = true
+}
+
+dependencies {
+    implementation("androidx.benchmark:benchmark-macro-junit$benchmarkVersion")
+    implementation("androidx.test.espresso:espresso-core:3.7.0")
+    implementation("androidx.test.ext:junit:1.3.0")
+    implementation("androidx.test.uiautomator:uiautomator:2.3.0")
+}
+
+
+androidComponents {
+    onVariants { v ->
+        val artifactsLoader = v.artifacts.getBuiltArtifactsLoader()
+        v.instrumentationRunnerArguments.put(
+            "targetAppId",
+            v.testedApks.map { artifactsLoader.load(it)?.applicationId }
+        )
+    }
+}
