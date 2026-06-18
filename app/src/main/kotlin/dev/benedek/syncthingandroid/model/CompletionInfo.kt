2bed590c6a8fe6daf742527d40272b7528181ad5
Benedek Szabó <bndkszabo@gmail.com>
2026-06-18 16:00:00 +0200

    Move to data class, fix docs and update fields

--- app/src/main/kotlin/dev/benedek/syncthingandroid/model/CompletionInfo.kt
+++ app/src/main/kotlin/dev/benedek/syncthingandroid/model/CompletionInfo.kt
@@ -7,17 +7,33 @@ package dev.benedek.syncthingandroid.model
  * 
  * completion is also returned by the events API
  * https://docs.syncthing.net/events/foldercompletion.html
- * 
+ *
+ * @param completion The completion percentage for the folder/device pair from 0.0 to 100.0.
+ *
+ *
+ * The following values aren't used currently.
+ * TODO: Implement the ui for them.
+ *
+ * @param globalBytes TODO
+ * @param globalItems TODO
+ * @param needBytes TODO
+ * @param needDeletes TODO
+ * @param needItems TODO
+ * @param remoteFolderState TODO
+ * @param sequence TODO
  */
-class CompletionInfo {
-	var completion: Double = 100.0
-	/**
-	 * The following values are only returned by the REST API call
-	 * to ""/completion". We will need them in the future to show
-	 * more statistics in the device UI.
-	 */
-	// public long globalBytes = 0;
-	// public long needBytes = 0;
-	// public long needDeletes = 0;
-	// public long needItems = 0;
-}
+data class CompletionInfo (
+	var completion: Double = 0.0, // CompletionPct float64
+
+	var globalBytes: Long = 0L, // GlobalBytes   int64
+	var globalItems: Int = 0, // GlobalItems   int
+	var needBytes: Long = 0L, // NeedBytes     int64
+	var needDeletes: Int = 0, // NeedDeletes   int
+	var needItems: Int = 0, // NeedItems     int
+	var remoteFolderState: RemoteFolderState = RemoteFolderState.UNKNOWN, // RemoteState   remoteFolderState
+	var sequence: Long = 0L, // Sequence      int64
+
+)
+
+
+
