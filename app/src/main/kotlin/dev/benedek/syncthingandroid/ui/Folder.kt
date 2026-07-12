0efc189ef59d26d7d276575673af569ac7fbbc7d
Benedek Szabó <bndkszabo@gmail.com>
2026-07-12 23:06:25 +0200

    Fix VM in preview

--- app/src/main/kotlin/dev/benedek/syncthingandroid/ui/Folder.kt
+++ app/src/main/kotlin/dev/benedek/syncthingandroid/ui/Folder.kt
@@ -63,6 +63,7 @@ import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.preference.PreferenceManager
 import dev.benedek.syncthingandroid.R
 import dev.benedek.syncthingandroid.activities.FolderPickerActivity
@@ -579,6 +580,6 @@ fun VersioningDialog(
 @Preview(showSystemUi = true, showBackground = true, uiMode = ThemeControls.UI_MODE)
 fun FolderPreview() {
 	SyncthingandroidTheme(ThemeControls.useDarkMode, dynamicColor = ThemeControls.isMonetEnabled) {
-		Folder(FolderViewModel())
+		Folder(viewModel<FolderViewModel>())
 	}
 }
