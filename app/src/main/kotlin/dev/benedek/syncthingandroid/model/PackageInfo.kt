62b1086e6672aa38bcb91920c03ce13caf57754c
Benedek Szabó <bndkszabo@gmail.com>
2026-07-16 11:48:52 +0200

    Add PackageInfo object
    I want to migrate to this over BuildConfig in the long term if possible.

---
+++ app/src/main/kotlin/dev/benedek/syncthingandroid/model/PackageInfo.kt
@@ -0,0 +1,13 @@
+package dev.benedek.syncthingandroid.model
+
+/**
+ * TODO: WRITE KDOC AND MIGRATE TO THIS OVER BuildConfig
+ */
+object PackageInfo {
+	var DEBUG: Boolean? = null
+	var APPLICATION_ID: String? = null
+	var BUILD_TYPE: String? = null
+	var VERSION_CODE: Int? = null
+	var VERSION_NAME: String? = null
+}
+
