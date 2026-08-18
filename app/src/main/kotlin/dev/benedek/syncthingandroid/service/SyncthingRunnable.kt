a57b499264df9723b78f462369a90f23946340e0
Benedek Szabó <bndkszabo@gmail.com>
2026-08-18 14:26:42 +0200

    refactor: Remove unused dependencies

--- app/src/main/kotlin/dev/benedek/syncthingandroid/service/SyncthingRunnable.kt
+++ app/src/main/kotlin/dev/benedek/syncthingandroid/service/SyncthingRunnable.kt
@@ -16,14 +16,11 @@ import androidx.preference.PreferenceManager
 import dev.benedek.syncthingandroid.R
 import dev.benedek.syncthingandroid.util.Util
 import eu.chainfire.libsuperuser.Shell
-import java.io.BufferedReader
 import java.io.DataOutputStream
 import java.io.File
 import java.io.FileWriter
 import java.io.IOException
 import java.io.InputStream
-import java.io.InputStreamReader
-import java.nio.charset.StandardCharsets
 import java.security.InvalidParameterException
 import java.util.concurrent.atomic.AtomicReference
 
