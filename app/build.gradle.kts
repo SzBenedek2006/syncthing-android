7f540f4f131264bcfe7abbf203790f0033bdd376
Benedek Szabó <bndkszabo@gmail.com>
2026-09-17 09:50:10 +0200

    Increment version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,8 +84,8 @@ configure<ApplicationExtension> {
 		applicationId = "dev.benedek.syncthingandroid"
 		minSdk = libs.versions.minSdk.get().toInt()
 		targetSdk = libs.versions.targetSdk.get().toInt()
-		versionCode = 4800
-		versionName = "2.1.5.0"
+		versionCode = 4801
+		versionName = "2.1.5.1"
 		testApplicationId = "dev.benedek.syncthingandroid.test"
 		manifestPlaceholders["appName"] = "@string/app_name"
 		//testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
