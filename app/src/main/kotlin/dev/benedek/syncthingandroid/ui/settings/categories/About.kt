6bf6886595035ac6b40b21fd28f42b13d828fdbf
Benedek Szabó <bndkszabo@gmail.com>
2026-09-20 10:29:43 +0200

    feat: About page redesign 1

--- app/src/main/kotlin/dev/benedek/syncthingandroid/ui/settings/categories/About.kt
+++ app/src/main/kotlin/dev/benedek/syncthingandroid/ui/settings/categories/About.kt
@@ -1,15 +1,26 @@
 package dev.benedek.syncthingandroid.ui.settings.categories
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.benedek.syncthingandroid.R
 import dev.benedek.syncthingandroid.ui.reusable.preventClicksWhenExiting
@@ -33,6 +44,27 @@ fun About(contentPadding: PaddingValues, viewModel: SettingsViewModel) {
 			.preventClicksWhenExiting(),
 		contentPadding = contentPadding
 	) {
+		item {
+			Column(
+				Modifier
+					.fillMaxWidth()
+					.padding(24.dp),
+				Arrangement.Center,
+				Alignment.CenterHorizontally
+			) {
+				Icon(
+					painterResource(R.drawable.ic_syncthing_monochrome),
+					null,
+					Modifier.fillMaxWidth(0.5f).aspectRatio(1f).padding(16.dp),
+					MaterialTheme.colorScheme.primary
+				)
+				Text(
+					"Syncthing for Android",
+					textAlign = TextAlign.Center,
+					style = MaterialTheme.typography.displaySmall
+				)
+			}
+		}
 		preference(
 			key = "syncthing_forum",  // #noKey
 			title = { Text(stringResource(R.string.syncthing_forum_title)) },
