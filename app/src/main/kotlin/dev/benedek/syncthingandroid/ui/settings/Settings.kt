e3c7530d9a199fd34f5df1a4caccdee8e4d50215
Benedek Szabó <bndkszabo@gmail.com>
2026-09-21 14:15:44 +0200

    fix: Make settings scrollable

--- app/src/main/kotlin/dev/benedek/syncthingandroid/ui/settings/Settings.kt
+++ app/src/main/kotlin/dev/benedek/syncthingandroid/ui/settings/Settings.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.Rule
 import androidx.compose.material.icons.outlined.BugReport
@@ -187,6 +189,7 @@ fun Settings(
 				Column(
 					modifier = Modifier
 						.fillMaxSize()
+						.verticalScroll(rememberScrollState())
 						.preventClicksWhenExiting()
 						.padding(contentPadding)
 				) {
