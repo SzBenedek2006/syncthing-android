5d8d389ab9a2d24c50a4c143e07df334d1c2113f
Benedek Szabó <bndkszabo@gmail.com>
2026-09-21 13:23:45 +0200

    refactor: Add new syncthing monochrome icon (compose)

---
+++ app/src/main/kotlin/dev/benedek/syncthingandroid/ui/icons/SyncthingForAndroidNew.kt
@@ -0,0 +1,130 @@
+@file:Suppress("ObjectPropertyName", "BooleanLiteralArgument")
+
+package dev.benedek.syncthingandroid.ui.icons
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType.Companion.EvenOdd
+import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap.Companion.Round
+import androidx.compose.ui.graphics.StrokeCap.Companion.Square
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.ImageVector.Builder
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlin.Unit
+
+val SyncthingForAndroidNew: ImageVector
+    get() {
+        if (_syncthingForAndroidNew != null) {
+            return _syncthingForAndroidNew!!
+        }
+        _syncthingForAndroidNew = Builder(
+            name = "Syncthing for android new mono fixed",
+            defaultWidth = 24.0.dp,
+            defaultHeight = 24.0.dp,
+            viewportWidth = 128.0f,
+            viewportHeight = 128.0f
+        ).apply {
+            path(
+                fill = SolidColor(Color(0xFFffffff)),
+                stroke = SolidColor(Color(0xFF000000)),
+                fillAlpha = 0.0f,
+                strokeLineWidth = 7.9989f,
+                strokeLineCap = Square,
+                strokeLineJoin = Miter,
+                strokeLineMiter = 4.0f,
+                pathFillType = NonZero
+            ) {
+                moveTo(30.83f, 78.22f)
+                arcTo(10.21f, 10.22f, 0.0f, false, true, 20.61f, 88.44f)
+                arcTo(10.21f, 10.22f, 0.0f, false, true, 10.4f, 78.22f)
+                arcTo(10.21f, 10.22f, 0.0f, false, true, 20.61f, 68.0f)
+                arcTo(10.21f, 10.22f, 0.0f, false, true, 30.83f, 78.22f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFFffffff)), stroke = SolidColor(Color(0xFF000000)),
+                    fillAlpha = 0.0f, strokeLineWidth = 7.9989f, strokeLineCap = Square,
+                    strokeLineJoin = Miter, strokeLineMiter = 4.0f, pathFillType = NonZero) {
+                moveTo(83.09f, 70.22f)
+                arcTo(10.21f, 10.22f, 0.0f, false, true, 72.88f, 80.44f)
+                arcTo(10.21f, 10.22f, 0.0f, false, true, 62.67f, 70.22f)
+                arcTo(10.21f, 10.22f, 0.0f, false, true, 72.88f, 60.0f)
+                arcTo(10.21f, 10.22f, 0.0f, false, true, 83.09f, 70.22f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFFffffff)), stroke = SolidColor(Color(0xFF000000)),
+                    fillAlpha = 0.0f, strokeLineWidth = 7.9989f, strokeLineCap = Square,
+                    strokeLineJoin = Miter, strokeLineMiter = 4.0f, pathFillType = NonZero) {
+                moveToRelative(102.56f, 99.02f)
+                arcToRelative(10.21f, 10.22f, 0.0f, false, true, -10.21f, 10.22f)
+                arcToRelative(10.21f, 10.22f, 0.0f, false, true, -10.21f, -10.22f)
+                arcToRelative(10.21f, 10.22f, 0.0f, false, true, 10.21f, -10.22f)
+                arcToRelative(10.21f, 10.22f, 0.0f, false, true, 10.21f, 10.22f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFFffffff)), stroke = SolidColor(Color(0xFF000000)),
+                    fillAlpha = 0.0f, strokeLineWidth = 7.9989f, strokeLineCap = Square,
+                    strokeLineJoin = Miter, strokeLineMiter = 4.0f, pathFillType = NonZero) {
+                moveTo(114.93f, 45.42f)
+                arcTo(10.21f, 10.22f, 0.0f, false, true, 104.72f, 55.64f)
+                arcTo(10.21f, 10.22f, 0.0f, false, true, 94.51f, 45.42f)
+                arcTo(10.21f, 10.22f, 0.0f, false, true, 104.72f, 35.2f)
+                arcTo(10.21f, 10.22f, 0.0f, false, true, 114.93f, 45.42f)
+                close()
+            }
+            path(fill = SolidColor(Color(0x00000000)), stroke = SolidColor(Color(0xFF000000)),
+                    strokeLineWidth = 8.0f, strokeLineCap = Round, strokeLineJoin =
+                    StrokeJoin.Companion.Round, strokeLineMiter = 4.0f, pathFillType = EvenOdd) {
+                moveToRelative(78.6f, 78.68f)
+                lineToRelative(8.02f, 11.87f)
+            }
+            path(fill = SolidColor(Color(0x00000000)), stroke = SolidColor(Color(0xFF000000)),
+                    strokeLineWidth = 8.0f, strokeLineCap = Round, strokeLineJoin =
+                    StrokeJoin.Companion.Round, strokeLineMiter = 4.0f, pathFillType = EvenOdd) {
+                moveToRelative(30.71f, 76.67f)
+                lineToRelative(32.07f, -4.91f)
+            }
+            path(fill = SolidColor(Color(0x00000000)), stroke = SolidColor(Color(0xFF000000)),
+                    strokeLineWidth = 8.0f, strokeLineCap = Round, strokeLineJoin =
+                    StrokeJoin.Companion.Round, strokeLineMiter = 4.0f, pathFillType = EvenOdd) {
+                moveTo(80.94f, 63.94f)
+                lineTo(96.66f, 51.69f)
+            }
+            path(fill = SolidColor(Color(0x00000000)), stroke = SolidColor(Color(0xFF000000)),
+                    fillAlpha = 0.0f, strokeLineWidth = 8.26667f, strokeLineCap = Round,
+                    strokeLineJoin = StrokeJoin.Companion.Round, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveToRelative(18.38f, 64.15f)
+                curveToRelative(-0.0f, -0.24f, 0.02f, 0.1f, 0.02f, -0.14f)
+                curveToRelative(0.0f, -24.96f, 20.23f, -45.19f, 45.19f, -45.19f)
+                curveToRelative(13.05f, -0.0f, 24.98f, 5.61f, 33.3f, 14.66f)
+                moveToRelative(11.63f, 25.65f)
+                curveToRelative(0.17f, 1.6f, 0.26f, 3.23f, 0.26f, 4.88f)
+                horizontalLineToRelative(-0.0f)
+                curveToRelative(0.0f, 8.94f, -2.6f, 17.27f, -7.08f, 24.29f)
+                moveToRelative(-21.67f, 17.82f)
+                curveToRelative(-5.1f, 1.99f, -10.64f, 3.09f, -16.45f, 3.09f)
+                curveToRelative(-14.91f, 0.0f, -28.14f, -7.22f, -36.37f, -18.36f)
+            }
+        }.build()
+        return _syncthingForAndroidNew!!
+    }
+
+private var _syncthingForAndroidNew: ImageVector? = null
+
+@Preview
+@Composable
+private fun Preview(): Unit {
+    Box(modifier = Modifier.padding(12.dp)) {
+        Image(imageVector = SyncthingForAndroidNew, contentDescription = "")
+    }
+}
