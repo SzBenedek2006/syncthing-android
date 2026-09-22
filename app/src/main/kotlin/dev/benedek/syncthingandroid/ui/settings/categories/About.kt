a0e843a3eb038ef9c33f96851d7b8be091d4e589
Benedek Szabó <bndkszabo@gmail.com>
2026-09-22 10:57:30 +0200

    feat: Richer about page in settings

--- app/src/main/kotlin/dev/benedek/syncthingandroid/ui/settings/categories/About.kt
+++ app/src/main/kotlin/dev/benedek/syncthingandroid/ui/settings/categories/About.kt
@@ -3,11 +3,13 @@ package dev.benedek.syncthingandroid.ui.settings.categories
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -17,7 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,6 +27,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.benedek.syncthingandroid.R
 import dev.benedek.syncthingandroid.ui.icons.SyncthingForAndroidNew
+import dev.benedek.syncthingandroid.ui.reusable.OptionTile
 import dev.benedek.syncthingandroid.ui.reusable.preventClicksWhenExiting
 import dev.benedek.syncthingandroid.ui.theme.SyncthingandroidTheme
 import dev.benedek.syncthingandroid.util.ThemeControls
@@ -40,13 +43,17 @@ fun About(contentPadding: PaddingValues, viewModel: SettingsViewModel) {
 	val donateUrl = stringResource(R.string.donate_url)
 	val privacyUrl = stringResource(R.string.privacy_policy_url)
 
-	LazyColumn(
+	val windowInfo = LocalWindowInfo.current
+	val isLandscape = windowInfo.containerSize.width > windowInfo.containerSize.height
+
+	Column(
 		modifier = Modifier
 			.fillMaxSize()
-			.preventClicksWhenExiting(),
-		contentPadding = contentPadding
+			.preventClicksWhenExiting()
+			.verticalScroll(rememberScrollState())
+			.padding(contentPadding)
 	) {
-		item {
+		if (!isLandscape) {
 			Column(
 				Modifier
 					.fillMaxWidth()
@@ -66,47 +73,91 @@ fun About(contentPadding: PaddingValues, viewModel: SettingsViewModel) {
 					style = MaterialTheme.typography.displaySmall
 				)
 			}
+		} else {
+			Row(
+				Modifier
+					.fillMaxWidth()
+					.padding(24.dp),
+				Arrangement.Center,
+				Alignment.CenterVertically
+			) {
+				Icon(
+					rememberVectorPainter(SyncthingForAndroidNew),
+					null,
+					Modifier.fillMaxWidth(0.25f).aspectRatio(1f).padding(16.dp),
+					MaterialTheme.colorScheme.primary
+				)
+				Text(
+					"Syncthing for Android",
+					textAlign = TextAlign.Center,
+					style = MaterialTheme.typography.displaySmall
+				)
+			}
 		}
-		preference(
-			key = "syncthing_forum",  // #noKey
-			title = { Text(stringResource(R.string.syncthing_forum_title)) },
-			summary = { Text(stringResource(R.string.syncthing_forum_summary)) },
-			onClick = { uriHandler.openUri(forumUrl) }
+		OptionTile(
+			title = stringResource(R.string.syncthing_forum_title),
+			description = stringResource(R.string.syncthing_forum_summary),
+			titleStyle = MaterialTheme.typography.bodyLarge,
+			descriptionStyle = MaterialTheme.typography.bodyMedium,
+			onClick = { uriHandler.openUri(forumUrl) },
+			noIconPadding = true
 		)
-		preference(
-			key = "report_issue",  // #noKey
-			title = { Text(stringResource(R.string.report_issue_title)) },
-			summary = { Text(stringResource(R.string.report_issue_summary)) },
-			onClick = { uriHandler.openUri(issuesUrl) }
+		OptionTile(
+			title = stringResource(R.string.report_issue_title),
+			description = stringResource(R.string.report_issue_summary),
+			titleStyle = MaterialTheme.typography.bodyLarge,
+			descriptionStyle = MaterialTheme.typography.bodyMedium,
+			onClick = { uriHandler.openUri(issuesUrl) },
+			noIconPadding = true
 		)
-		preference(
-			key = "donate",  // #noKey
-			title = { Text(stringResource(R.string.donate_title)) },
-			summary = { Text(stringResource(R.string.donate_summary)) },
-			onClick = { uriHandler.openUri(donateUrl) }
+		OptionTile(
+			title = stringResource(R.string.donate_title),
+			description = stringResource(R.string.donate_summary),
+			titleStyle = MaterialTheme.typography.bodyLarge,
+			descriptionStyle = MaterialTheme.typography.bodyMedium,
+			onClick = { uriHandler.openUri(donateUrl) },
+			noIconPadding = true
 		)
-		preference(
-			key = "privacy",  // #noKey
-			title = { Text(stringResource(R.string.privacy_title)) },
-			summary = { Text(stringResource(R.string.privacy_summary)) },
-			onClick = { uriHandler.openUri(privacyUrl) }
+		OptionTile(
+			title = stringResource(R.string.privacy_title),
+			description = stringResource(R.string.privacy_summary),
+			titleStyle = MaterialTheme.typography.bodyLarge,
+			descriptionStyle = MaterialTheme.typography.bodyMedium,
+			onClick = { uriHandler.openUri(privacyUrl) },
+			noIconPadding = true
 		)
-		preference(
-			key = "syncthing_version",
-			title = { Text(stringResource(R.string.syncthing_version_title)) },
-			summary = { Text(viewModel.syncthingVersion.value) }
+		OptionTile(
+			title = stringResource(R.string.syncthing_version_title),
+			description = viewModel.syncthingVersion.value,
+			titleStyle = MaterialTheme.typography.bodyLarge,
+			descriptionStyle = MaterialTheme.typography.bodyMedium,
+			noIconPadding = true
 		)
-		preference(
-			key = "app_version",
-			title = { Text(stringResource(R.string.app_version_title)) },
-			summary = { Text(viewModel.syncthingAppVersion.value) }
+		OptionTile(
+			title = stringResource(R.string.app_version_title),
+			description = viewModel.syncthingAppVersion.value,
+			titleStyle = MaterialTheme.typography.bodyLarge,
+			descriptionStyle = MaterialTheme.typography.bodyMedium,
+			noIconPadding = true
 		)
 	}
 }
 
 @Preview(showBackground = true, uiMode = ThemeControls.UI_MODE)
 @Composable
-fun AboutPreview() {
+fun AboutVerticalPreview() {
+	SyncthingandroidTheme(ThemeControls.PREVIEW_DARK_THEME) {
+		ProvidePreferenceLocals {
+			Scaffold { paddingValues ->
+				About(paddingValues, viewModel())
+			}
+		}
+	}
+}
+
+@Preview(showBackground = true, uiMode = ThemeControls.UI_MODE, widthDp = 800, heightDp = 400)
+@Composable
+fun AboutHorizontalPreview() {
 	SyncthingandroidTheme(ThemeControls.PREVIEW_DARK_THEME) {
 		ProvidePreferenceLocals {
 			Scaffold { paddingValues ->
